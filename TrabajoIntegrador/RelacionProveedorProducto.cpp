904241e0a1842298a5550171985c6a1883c7b93f
Fabricio Damian Di Paola <111092778+FabricioDDev@users.noreply.github.com>
2025-06-18 09:32:56 -0300

    se agrego campo id a la clase proveedorproducto

--- TrabajoIntegrador/RelacionProveedorProducto.cpp
+++ TrabajoIntegrador/RelacionProveedorProducto.cpp
@@ -3,18 +3,26 @@
 using namespace std;
 
 RelacionProveedorProducto::RelacionProveedorProducto(){
+    _idProveedorProducto=0;
     strcpy(_cuitProveedor,"");
     _idProducto = 0;
 }
-RelacionProveedorProducto::RelacionProveedorProducto(std::string cuitProveedor, int idProducto){
+RelacionProveedorProducto::RelacionProveedorProducto(int id, std::string cuitProveedor, int idProducto){
+    setIdProveedorProducto(id);
     setCuitProveedor(cuitProveedor);
     setIdProducto(idProducto);
+}
+int RelacionProveedorProducto::getIdProveedorProducto(){
+    return _idProveedorProducto;
 }
 std::string RelacionProveedorProducto::getCuitProveedor(){
     return _cuitProveedor;
 }
 int RelacionProveedorProducto::getIdProducto(){
     return _idProducto;
+}
+void RelacionProveedorProducto::setIdProveedorProducto(int id){
+    _idProveedorProducto=id;
 }
 void RelacionProveedorProducto::setCuitProveedor(std::string cuitProveedor){
     strcpy(_cuitProveedor, cuitProveedor.c_str());
--- TrabajoIntegrador/RelacionProveedorProducto.h
+++ TrabajoIntegrador/RelacionProveedorProducto.h
@@ -3,15 +3,18 @@
 
 class RelacionProveedorProducto{
     private:
+        int _idProveedorProducto;
         char _cuitProveedor[20];
         int _idProducto;
     public:
         RelacionProveedorProducto();
-        RelacionProveedorProducto(std::string cuitProveedor, int idProducto);
+        RelacionProveedorProducto(int idProveedorProducto, std::string cuitProveedor, int idProducto);
 
+        int getIdProveedorProducto();
         std::string getCuitProveedor();
         int getIdProducto();
 
+        void setIdProveedorProducto(int id);
         void setCuitProveedor(std::string cuitProveedor);
         void setIdProducto(int idProducto);
 
